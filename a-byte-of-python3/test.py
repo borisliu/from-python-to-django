c0b57c135e147da379fcd09278e70ecafde5932c
borisliu <boris_cn@263.net>
2020-04-05 20:37:30 +0800

    test.py

--- a-byte-of-python3/test.py
+++ a-byte-of-python3/test.py
@@ -1,15 +1,3 @@
-# 这是一个字符串对象
-name = 'Swaroop' 
+DEBUG=True
 
-if name.startswith('Swa'):
-    print('是的，字符串以"Swa"开始')
-
-if 'a' in name:
-    print('是的，它包含字符串"a"')
-
-if name.find('war') != -1:
-    print('是的，它包含字符串"war"')
-
-delimiter = '_*_'
-mylist = ['巴西', '俄罗斯', '印度', '中国']
-print(delimiter.join(mylist))
+if DEBUG: print('aaa')
